60dc6f8a5dab4670fbc804cb6cfbd1cb5cc2088b
Kai-Chun Hsu <mattandkevin1060@gmail.com>
2020-05-22 22:38:56 -0400

    add extractions and index as params

--- scripts/crossword.js
+++ scripts/crossword.js
@@ -1,22 +1,27 @@
 const fontSize = 18;
 const indexSize = 12;
 const gridSize = 30;
+const redColor = "#FF0000";
 
-const crossword = words => {
-  const crosswordHTML = words.reduce((res, next, index) => {
-    const { x, y, word, direction } = next;
-    return `
-    ${res}
-    <span style="
-      position: absolute;
-      top: ${x * gridSize + 1}px;
-      left: ${y * gridSize + 1}px;
-      font-size: ${indexSize}px"
-      >
-      ${index + 1}
-    </span>
-    ${processWord(x, y, word, direction)}`;
-  }, "");
+const crossword = (words, extractions = []) => {
+  const crosswordHTML = words
+    .reduce((res, next) => {
+      const { index, top, left, word, direction } = next;
+      return `
+      ${res}
+      <span style="
+        position: absolute;
+        z-index: 1000;
+        top: ${top * gridSize + 1}px;
+        left: ${left * gridSize + 1}px;
+        font-size: ${indexSize}px"
+        >
+        ${index}
+      </span>
+      ${processWord(top, left, word, direction)}
+    `;
+    }, "")
+    .concat(processExtractions(extractions));
 
   const target = document.getElementById("target");
   target.style =
@@ -24,35 +29,35 @@ const crossword = words => {
   target.innerHTML = crosswordHTML;
 };
 
-const processWord = (x, y, word, direction) => {
+const processWord = (top, left, word, direction) => {
   return word.split("").reduce(
     (res, letter, index) =>
       `${res}
-        ${processLetter(x, y, letter, direction, index)}`,
+        ${processLetter(top, left, letter, direction, index)}`,
     ""
   );
 };
 
-const processLetter = (x, y, letter, direction, index) => {
-  const posX =
-    x * gridSize +
+const processLetter = (top, left, letter, direction, index) => {
+  const posTop =
+    top * gridSize +
     (direction === "up" ? -1 : direction === "down" ? 1 : 0) * index * gridSize;
-  const posY =
-    y * gridSize +
+  const posLeft =
+    left * gridSize +
     (direction === "right" ? 1 : direction === "left" ? -1 : 0) *
       index *
       gridSize;
 
   return letter === "_"
-    ? printInputBox(posX, posY)
-    : printLetter(posX, posY, letter);
+    ? printInputBox(posTop, posLeft)
+    : printLetter(posTop, posLeft, letter);
 };
 
-const printLetter = (x, y, l) =>
+const printLetter = (top, left, l) =>
   `<p style="
       position: absolute;
-      top: ${x};
-      left: ${y};
+      top: ${top};
+      left: ${left};
       display: flex;
       align-items: center;
       justify-content: center;
@@ -68,13 +73,13 @@ const printLetter = (x, y, l) =>
       ${l}
     </p>`;
 
-const printInputBox = (x, y) =>
+const printInputBox = (top, left) =>
   `<input
     maxlength='1'
     style='
       position: absolute;
-      top: ${x};
-      left: ${y};
+      top: ${top};
+      left: ${left};
       width: ${gridSize}px;
       height: ${gridSize}px;
       padding: 0;
@@ -87,3 +92,21 @@ const printInputBox = (x, y) =>
       z-index: 1;
       box-sizing: border-box;'
     />`;
+
+processExtractions = extractions =>
+  extractions.reduce((res, next) => {
+    const { index, top, left } = next;
+    return `
+      ${res}
+      <span
+        style="position: absolute;
+        z-index: 1000;
+        top: ${top * gridSize + gridSize - 14}px;
+        left: ${left * gridSize + gridSize - 14}px;
+        font-size: 12px;
+        transform: scale(0.75);
+        color: ${redColor}"
+        >
+        (${index})
+      </span>`;
+  }, "");
